bf9f526956b67dae3ca3eb4f6f0aae0da4255a0c
Pontificust <lihtinmann88@yandex.ru>
2026-03-05 20:44:57 +0300

    feat: implement game logic

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import { game } from "./modules/index.js";
+
+window.addEventListener('DOMContentLoaded', () => {
+    game();
+});
---
+++ js/modules/game.js
@@ -0,0 +1,162 @@
+export const game = () => {
+    const winCombinations = [
+        '012',
+        '345',
+        '678',
+        '036',
+        '147',
+        '258',
+        '048',
+        '246'
+    ];
+
+    const Player = (name, token) => {
+        let moves = [];
+
+        function setMoves(x, y) {
+            moves.push(+x + +y * 3);
+        }
+
+        function getMoves() {
+            return moves;
+        }
+
+        function resetMoves() {
+            moves = [];
+        }
+
+        return { name, token, setMoves, getMoves, resetMoves };
+    }
+
+
+    const gameBoard = (() => {
+        let board = Array(3).fill(Array(3).fill(Cell()));
+
+        function getBoard() {
+            return board;
+        };
+
+        function setToken(player, x, y) {
+            board[x][y].setToken(player);
+            console.log(board[x][y].getToken())
+        }
+
+        function resetBoard() {
+            board = Array(3).fill(Array(3).fill(Cell()));
+        }
+
+        function printBoard() {
+            board.forEach(row => {
+                // row.forEach(cell => console.log(cell.getToken()));
+            });
+        }
+
+        return {
+            getBoard,
+            setToken,
+            resetBoard,
+            printBoard
+        }
+    })();
+
+    function Cell() {
+        let token = 0;
+
+        function setToken(player) {
+            token = player.token
+        };
+
+        function getToken(){
+            return token;
+        }
+
+        return { setToken, getToken }
+    }
+
+    const game = (() => {
+        let movesCounter = 0;
+        const gameState = {
+            isWin: false,
+            isDraw: false,
+            resetState() {
+                this.isDraw = false;
+                this.isWin = false;
+            }
+        }
+        const players = Array.from(Array(2), (x, idx) => Player(`player${idx}`, idx));
+
+        let currentPlayer = players[1];
+
+        function switchPlayer() {
+            currentPlayer = currentPlayer === players[1] ? players[0] : players[1];
+        }
+
+        function getCurrentPlayer() {
+            return currentPlayer;
+        }
+
+        function resetGame() {
+            gameBoard.resetBoard();
+            players.forEach(player => player.resetMoves());
+            currentPlayer = players[1];
+            movesCounter = 0;
+            gameState.resetState();
+        }
+
+        function printRound() {
+            gameBoard.printBoard();
+            console.log(`Current player is ${currentPlayer}.`);
+        }
+
+        function checkWinner() {
+            movesCounter += 1;
+            const playerMoves = currentPlayer.getMoves().sort().join('');
+            console.log(playerMoves)
+            if (movesCounter !== 9 && playerMoves.length === 3) {
+                if (winCombinations.some(val => playerMoves === val)) {
+                    gameState.isWin = true;
+                }
+            } else if (movesCounter === 9) {
+                gameState.isDraw = true;
+            }
+        }
+
+        function playRound(x, y) {
+
+            console.log(`Dropping ${currentPlayer.name}'s marker into the ${x},${y} cell...`);
+            currentPlayer.setMoves(x, y);
+            gameBoard.setToken(currentPlayer, x, y);
+
+            checkWinner();
+
+            let { isWin, isDraw } = gameState;
+
+            if (isWin || isDraw) {
+                if (isWin) {
+                    console.log(`Dear, ${currentPlayer.name}, you win!`);
+                } else if (isDraw) {
+                    console.log("It's a draw!");
+                }
+                resetGame();
+                return false;
+            }
+            switchPlayer();
+            printRound();
+
+            return true;
+        }
+
+        return { playRound, getCurrentPlayer };
+    })()
+
+    document.addEventListener('click', () => {
+        let control = true;
+    
+        while(control){
+            let userInput = prompt("Input coordinates");
+            let [x, y] = userInput.split(' ');
+            control = game.playRound(x, y);
+        }
+    });
+
+}
